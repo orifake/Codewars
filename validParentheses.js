661b596c708dba04f0e26de1416046d904a9bc56
Yujingyi <349758699@qq.com>
2020-05-08 23:40:37 +0800

    update

---
+++ validParentheses.js
@@ -0,0 +1,32 @@
+/*
+    Valid Parentheses
+
+    Description:
+    Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+    Examples
+    "()"              =>  true
+    ")(()))"          =>  false
+    "("               =>  false
+    "(())((()())())"  =>  true
+*/
+
+const validParentheses = function (parens) {
+  let openingBrackets = ['{', '[', '(']
+  let closingBrackets = ['}', ']', ')']
+
+  let stack = []
+
+  for (let i = 0; i < parens.length - 1; i++) {
+    let ch = parens[i];
+    if (closingBrackets.indexOf(ch) > -1) {
+      let matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) {
+        return false
+      }
+    } else {
+      stack.push(ch);
+    }
+  }
+  return stack.length === 0
+}
